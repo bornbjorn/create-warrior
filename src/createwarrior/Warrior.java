ce137ba67337b7e66151c317777d7b8eb5671d93
Andre Swartz <andre.swartz@standardbank.co.za>
2018-10-26 12:35:36 +0200

    commit

--- src/createwarrior/Warrior.java
+++ src/createwarrior/Warrior.java
@@ -27,76 +27,43 @@ public abstract class Warrior {
     
     public abstract void getName();
 
-    /**
-     * @return the origin
-     */
     public String getOrigin() {
         return origin;
     }
 
-    /**
-     * @param bodyType the bodyType to set
-     */
     public void setBodyType(String bodyType) {
         this.bodyType = bodyType;
     }
 
-    /**
-     * @param physicalStrength the physicalStrength to set
-     */
     public void setPhysicalStrength(int physicalStrength) {
         this.physicalStrength = physicalStrength;
     }
 
-    /**
-     * @param speed the speed to set
-     */
     public void setSpeed(int speed) {
         this.speed = speed;
     }
 
-    /**
-     * @param agility the agility to set
-     */
     public void setAgility(int agility) {
         this.agility = agility;
     }
 
-    /**
-     * @param martialArtsType the martialArtsType to set
-     */
     public void setMartialArtsType(String martialArtsType) {
         this.martialArtsType = martialArtsType;
     }
 
-    /**
-     * @param weapons the weapons to set
-     */
     public void setWeapons(String weapons) {
         this.weapons = weapons;
     }
 
-    /**
-     * @param powers the powers to set
-     */
     public void setPowers(String powers) {
         this.powers = powers;
     }
 
-    /**
-     * @param specialSkill the specialSkill to set
-     */
     public void setSpecialSkill(String specialSkill) {
         this.specialSkill = specialSkill;
     }
 
-    /**
-     * @param levelUpAbility the levelUpAbility to set
-     */
     public void setLevelUpAbility(boolean levelUpAbility) {
         this.levelUpAbility = levelUpAbility;
     }
-    
-
-    
 }
